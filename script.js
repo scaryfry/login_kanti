7f978a9bd571981266743b862b47ce168bac7870
scaryfry <scaryfry@gmail.com>
2025-09-12 10:18:07 +0200

    finished

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const data = [];
 function loginConfirm(){
+    const email = document.getElementById('email-login').value;
+    const password = document.getElementById('password-login').value;
 
+    console.log(email, password)
+    if(sessionStorage.getItem('email') == email && sessionStorage.getItem('password') == password){
+        alert('Sikeres bejelentkezés');
+        document.getElementById('email-login').value = '';
+        document.getElementById('password-login').value = '';
+        return;
+    }
+    else{
+        alert('A jelszó vagy az email nem helyes');
+        return;
+    }
 }
 function showRegisterPage(){
     document.getElementById("password-register").type = "password";
@@ -25,11 +38,17 @@ document.getElementById('register-button').addEventListener("click", () => {
     const email = document.getElementById('email-login').value;
     const password = document.getElementById('password-register').value;
     const repeat = document.getElementById('password-repeat').value;
-    if(!password == repeat){
-        alert('Nem egyezik meg a két jelszó!')
+    if(password != repeat){
+        alert('Nem egyezik meg a két jelszó!');
+        return;
     }
     data.push(email, password);
     console.log(data);
     alert('Sikeres a regisztráció');
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("password", password);
+    sessionStorage.setItem("repeat", repeat);
+    document.getElementById('password-register').value = '';
+    document.getElementById('password-repeat').value = '';
     showLoginPage();
 })
